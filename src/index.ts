176afcf25ef028c546868649b40d33f558056119
Dane Lipscombe <danelipscombe@gmail.com>
2022-04-25 11:22:57 +1000

    Add types

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
 import readline from 'readline';
 import {echo} from './echo';
 
-const commands = {
+type Command = {
+  onLine: (line: string) => void,
+  onClose: () => void,
+};
+
+const commands: Record<string, Command> = {
   echo,
 };
 
+/** Main function */
 function main() {
+  const commandName = process.argv[2];
+  if (!commandName) {
+    throw new Error(`Command missing in arguments`);
+  }
+  if (!commands.hasOwnProperty(commandName)) {
+    throw new Error(`Unknown command '${commandName}'`);
+  }
+
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false,
   });
 
-  rl.on('line', echo.onLine);
-  rl.on('close', echo.onClose);
+  const command = commands[commandName];
+
+  rl.on('line', command.onLine);
+  rl.on('close', command.onClose);
 }
 
 main();
